239831dfc012a459ba8bed5f48f75276740a482f
artemtitov1 <titov.artem02@gmail.com>
2023-06-30 10:21:15 +0300

    small cleaning

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
 from UI.Window import Window
 
-# test object
-book = {
-    'id': '1',
-    'author': 'author',
-    'title': 'title',
-    'genre': 'genre',
-    'price': 'price',
-    'publish_date': '01.01.01',
-    'description': 'description'
-}
-
 window = Window()
 window.mainloop()
